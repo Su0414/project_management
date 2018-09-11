3f37ce61b6a854d6876c3c2db6b9d38099fe33f9
Su0414 <susanaik.com>
2018-09-11 08:20:34 -0700

    generate user and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/show'
+  get 'sessions/destroy'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

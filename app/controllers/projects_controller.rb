b0f97c7b18beefe225125f7b7beeffae93e767be
Su0414 <susanaik.com>
2018-09-12 08:57:29 -0700

    add project index action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,22 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
+    if current_user.admin
+      @users = User.where(admin: false)
+    else 
+      @users = User.where(id: current_user.id)
+    end
   end
 
   def show
+    
   end
 
   def new
+    @project = Project.new
+  end
+
+  def create 
   end
 
   def edit

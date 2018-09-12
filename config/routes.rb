c0dfa330402cf8f0e870e44ef760601018caafc7
Su0414 <susanaik.com>
2018-09-12 08:01:38 -0700

    generate projects and tasks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
-  # get 'sessions/new'
-  # get 'sessions/create'
-  # get 'sessions/show'
-  # get 'sessions/destroy'
-  # get 'users/show'
-  # get 'users/new'
-  # get 'users/create'
-  # get 'users/edit'
-  # get 'users/update'
-  # get 'users/destroy'
-
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/edit'
+  get 'projects/index'
+  get 'projects/show'
+  get 'projects/new'
+  get 'projects/edit'
   root 'application#home'
+
   get '/signin', to: 'sessions#sign_in'
   post '/signin', to: 'sessions#create'
 
@@ -18,6 +16,7 @@ Rails.application.routes.draw do
   
   resources :users
   resources :projects
+  resources :tasks
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

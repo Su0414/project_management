7b949bf4a558228b34c4d459dcf3e213dad7c4e3
Su0414 <susanaik.com>
2018-09-14 08:42:34 -0700

    add route to mark task complete

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,13 @@ Rails.application.routes.draw do
   delete '/signout', to: 'sessions#destroy'
   
   resources :users
+  
   resources :projects do 
-      resources :tasks
+      resources :tasks do 
+        member do 
+          patch :complete
+        end 
+      end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

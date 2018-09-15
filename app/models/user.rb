3e9531de0638b98b76117ad35cf055d29b5e722d
Su0414 <susanaik.com>
2018-09-15 08:55:22 -0700

    add validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
+    validates :first_name, presence: true, length: { minimum: 3 }
+    validates :last_name, presence: true, length: { minimum: 2 }
+
+    validates :password, presence: true
+
+    validates :email, presence: true, uniqueness: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
     has_secure_password 
     
     has_many :tasks

7fd39278c9e081b1fb3c07a700c5f10908b498f8
Su0414 <susanaik.com>
2018-09-15 10:19:04 -0700

    add project scope method by_status, recent

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
     has_many :tasks
     has_many :users, through: :tasks
 
-    scope :status, -> { where status: "active" }
+    scope :by_status, -> { where status: "active" }
+    scope :recent, -> { order("projects.updated_at DESC") }
 
   end

2ae9220148c3304d5a30796c55e24f9ec973187c
Su0414 <susanaik.com>
2018-11-12 06:36:55 -0800

    add ajax complete and delete for task

--- app/assets/javascripts/task.js
+++ app/assets/javascripts/task.js
@@ -12,35 +12,44 @@ $(function(){
         
         renderTd(){
             let formatTdhtml = "" ;
-            formatTdhtml += `<table>`; 
+            formatTdhtml += `                            
+                             <table>`; 
             this.format_tasks.forEach(task => {
             
                 formatTdhtml += `<tr>
-                                    <td data-id=${task.id}> `
+                                    <td data-id=${task.id}> 
+                                    <div class="task">`
 
                 if(task.completed_at!=null){
                     formatTdhtml += `<strike>${task.content}</strike>`;
                 }else{
                     formatTdhtml += `${task.content}`;
                 }            
-
-            
-                formatTdhtml += `</td>
-                                     <td data-id=${task.id}>
-                                        <a href="/projects/${task.project_id}/tasks/${task.id}/complete">
+           
+                formatTdhtml += `</div>
+                                    </td>
+                                     <td edit-id=${task.id}>`;
+                                        
+                
+                if(task.completed_at!=null){
+                    formatTdhtml += `<i style="opacity:0.4;" class="fa fa-check"></i>`;
+                }else{
+                    formatTdhtml += `<a href="/projects/${task.project_id}/tasks/${task.id}/complete", data-method="PATCH", class="complete_task">
                                         <div class="complete">
-                                            <i class="fa fa-check"></i>
-                                        </div>
-                                        </a>
-                                     </td>
-                                     <td data-id=${task.id}>
-                                        <a href="/projects/${task.project_id}/tasks/${task.id}/delete">
+                                            <i class="fa fa-check">
+                                        </i></div>
+                                    </a>`;
+                }
+
+                formatTdhtml += `</td>
+                                     <td delete-id=${task.id}>
+                                        <a href="/projects/${task.project_id}/tasks/${task.id}", data-method="DELETE", class="delete_task">
                                         <div class="trash">
                                             <i class="fa fa-trash"></i>
                                         </div>
                                         </a>
                                      </td>
-                                     </tr>`
+                                </tr>`;
             });
             formatTdhtml += `</table>`
             return formatTdhtml;
@@ -50,9 +59,6 @@ $(function(){
             let html = this.renderTd();
             $("div.projecttasks").append(html);  
         }
-
-        
-
     }
         
     // submitting completed project tasks through JS
@@ -105,5 +111,51 @@ $(function(){
             }
         });  
     });    
+
+    // submitting mark complete form through AJAX -Lower level
+    $('a.complete_task').on('click',function(e){   
+        alert(this.url);
+        e.preventDefault();
+        var url = this.href;
+        var data = $(this).serialize();
+        $.ajax({
+            type: "PATCH",
+            url: url, 
+            data: data,
+            success: function(response) { 
+                alert(response);
+            $("div.projecttasks").html('');
+            let type = "all";
+            let mytask = new Task(response, type);
+            let display_tasks = mytask.renderTasks();
+            $("div.projecttasks").append(display_tasks);
+            $("#task_content").val("");        
+                
+            }
+        });  
+    });    
+
+    // submitting delete form through AJAX -Lower level
+    $('a.delete_task').on('click', function(e){          
+        e.preventDefault();
+        var url = this.href;
+        var data = $(this).serialize();
+        $.ajax({
+            type: "DELETE",
+            url: url, 
+            data: data,
+            success: function(response) { 
+                
+            $("div.projecttasks").html('');
+            let type = "all";
+            let mytask = new Task(response, type);
+            let display_tasks = mytask.renderTasks();
+            $("div.projecttasks").append(display_tasks);
+            $("#task_content").val("");        
+                
+            }
+        });  
+    });    
+
 
 });
